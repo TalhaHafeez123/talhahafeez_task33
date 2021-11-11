166d4ab6d20ee0ea88f2620ccf90cd545bb3fcaa
TalhaHafeez123 <91986142+TalhaHafeez123@users.noreply.github.com>
2021-11-11 19:30:05 +0500

    main activity logic

---
+++ app/src/main/java/com/example/task3/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.task3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1, button2;
+        button1 = findViewById(R.id.quiz);
+        button2 = findViewById(R.id.git);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://github.com/arsl830/Task3");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
+    }
+}
